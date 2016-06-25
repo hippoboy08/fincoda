0459b86d47332b22e7dbbe3017573f046566e1d3
Shovit thapa <shovitthapa@gmail.com>
2016-06-25 11:26:37 +0300

    removed unused buttons

--- resources/views/sidebar/admin.blade.php
+++ resources/views/sidebar/admin.blade.php
@@ -37,8 +37,6 @@
                 <a href="#"><i class="fa fa-line-chart" aria-hidden="true"></i> <span>Survey</span> <i class="fa fa-angle-left pull-right"></i></a>
                 <ul class="treeview-menu">
                     <li><a href="{!! url('admin/survey/create') !!}"><i class="fa fa-list-alt" aria-hidden="true"></i>Create New</a></li>
-                    <li><a href="#"><i class="fa fa-spinner" aria-hidden="true"></i>Pending Survey</a></li>
-                    <li><a href="#"><i class="fa fa-square-o" aria-hidden="true"></i>Open Survey</a></li>
                     <li><a href="{{url('admin/survey')}}"><i class="fa fa-pie-chart" aria-hidden="true"></i>Survey Results</a></li>
 
                 </ul>
