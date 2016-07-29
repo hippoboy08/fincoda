6e3ff70ca071a5fac96e417c6056d67fe52fd3b7
Zenni <zhenkarilainen@gmail.com>
2016-07-29 14:07:38 +0300

    Homepage: CSS, context modified, links added

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -37,7 +37,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-          <a class="navbar-brand" href="#"><b>Fincoda</b></a>
+          <a class="navbar-brand" href="#"><img src="{{'landing_page/img/logo-white.png'}}" alt=""></a>
         </div>
         <div class="navbar-collapse collapse">
             <ul class="nav navbar-nav">
@@ -73,24 +73,26 @@
     <div class="container">
         <div class="row centered">
             <div class="col-lg-12">
-                <h1><b>Fincoda</b> Survey System</h1>
-                <h3>Is a tool for measuring intended learning outcomes in internal development activities for universities and working life organizations.</h3>
+                <h1>Fincoda Survey System</h1>
                 <br>
+                <h3>A tool for measuring intended learning outcomes in internal development activities for universities and working life organizations</h3>
+                <br><br>
             </div>
 
             <div class="col-lg-2">
                 <h5>Amazing Results</h5>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                <p>Scaled bar charts with all data fit in provide visual presentation of categorical data.</p>
                 <img class="hidden-xs hidden-sm hidden-md" src="{{'landing_page/img/arrow1.png'}}">
             </div>
             <div class="col-lg-8">
-                <img class="img-responsive" src="{{'landing_page/img/piechart2010.png'}}" alt="">
+                <img class="img-responsive" src="{{'landing_page/img/column.png'}}" alt="">
+                <br><br>
             </div>
             <div class="col-lg-2">
                 <br>
                 <img class="hidden-xs hidden-sm hidden-md" src="{{'landing_page/img/arrow2.png'}}">
                 <h5>Awesome Design</h5>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                <p>Column chart in survey report displays grouped data as vertical bars and show comparisons among categories.</p>
             </div>
         </div>
     </div> <!--/ .container -->
@@ -102,114 +104,63 @@
 <div id="intro">
     <div class="container">
         <div class="row centered">
-            <h1>Designed To Excel</h1>
+            <h1>FINCODA - Framework for innovation competencies <br>development and assessment</h1>
             <br>
             <br>
             <div class="col-lg-4">
-                <img src="{{'landing_page/img/intro01.png'}}" alt="">
-                <h3>Community</h3>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                <img src="{{'landing_page/img/survey.png'}}" alt="">
+                <h3>Features</h3>
+                <p>Internal development activities and<br>training services for staff</p>
             </div>
             <div class="col-lg-4">
-                <img src="{{'landing_page/img/intro02.png'}}" alt="">
-                <h3>Schedule</h3>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                <img src="{{'landing_page/img/employee.png'}}" alt="">
+                <h3>Benefits</h3>
+                <p>Improve the quality and efficiency of education and training</p>
             </div>
             <div class="col-lg-4">
-                <img src="{{'landing_page/img/intro03.png'}}" alt="">
-                <h3>Monitoring</h3>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                <img src="{{'landing_page/img/innovation.png'}}" alt="">
+                <h3>Objectives</h3>
+                <p>Innovation competence development and<br>assessment</p>
             </div>
         </div>
         <br>
-        <hr>
+
     </div> <!--/ .container -->
 </div><!--/ #introwrap -->
 
 <!-- FEATURES WRAP -->
 <div id="features">
     <div class="container">
-        <div class="row">
-            <h1 class="centered">What's New?</h1>
+        <div class="row centered">
+            <h1>FINCODA - production of remarkable tangible outputs</h1>
             <br>
             <br>
-            <div class="col-lg-6 centered">
-                <img class="centered" src="{{'landing_page/img/mobile.png'}}" alt="">
+            <div class="col-lg-7">
+                <img class="centered" src="{{'landing_page/img/thumbs.jpg'}}" alt="">
             </div>
-
-            <div class="col-lg-6">
-                <h3>Some Features</h3>
-                <br>
-                <!-- ACCORDION -->
-                <div class="accordion ac" id="accordion2">
-                    <div class="accordion-group">
-                        <div class="accordion-heading">
-                            <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapseOne">
-                                First Class Design
-                            </a>
-                        </div><!-- /accordion-heading -->
-                        <div id="collapseOne" class="accordion-body collapse in">
-                            <div class="accordion-inner">
-                                <p>It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-                            </div><!-- /accordion-inner -->
-                        </div><!-- /collapse -->
-                    </div><!-- /accordion-group -->
-                    <br>
-
-                    <div class="accordion-group">
-                        <div class="accordion-heading">
-                            <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapseTwo">
-                                Retina Ready Theme
-                            </a>
-                        </div>
-                        <div id="collapseTwo" class="accordion-body collapse">
-                            <div class="accordion-inner">
-                                <p>It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-                            </div><!-- /accordion-inner -->
-                        </div><!-- /collapse -->
-                    </div><!-- /accordion-group -->
-                    <br>
-
-                    <div class="accordion-group">
-                        <div class="accordion-heading">
-                            <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapseThree">
-                                Awesome Support
-                            </a>
-                        </div>
-                        <div id="collapseThree" class="accordion-body collapse">
-                            <div class="accordion-inner">
-                                <p>It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-                            </div><!-- /accordion-inner -->
-                        </div><!-- /collapse -->
-                    </div><!-- /accordion-group -->
-                    <br>
-
-                    <div class="accordion-group">
-                        <div class="accordion-heading">
-                            <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapseFour">
-                                Responsive Design
-                            </a>
-                        </div>
-                        <div id="collapseFour" class="accordion-body collapse">
-                            <div class="accordion-inner">
-                                <p>It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-                            </div><!-- /accordion-inner -->
-                        </div><!-- /collapse -->
-                    </div><!-- /accordion-group -->
-                    <br>
-                </div><!-- Accordion -->
+            <div class="col-lg-5 align-left">
+                <h3>FINCODA produces several remarkable tangible outputs:</h3>
+                    <p><br>
+                    &#10003 &nbsp A Toolkit for Behaviour Assessment: <br>
+                    Available as an electronic guide on the Internet<br>
+                    Can be used as a self learning material guide for training purposes<br><br>
+                    &#10003 &nbsp Software Application for Innovation Competencies Assessment<br><br>
+                    &#10003 &nbsp Massive Open Online Courses related to Behaviour Assessment<br><br>
+                    &#10003 &nbsp Rater Training Workshop<br><br>
+                    &#10003 &nbsp Innovation Competencies Assessment Workshops</p>
             </div>
         </div>
-    </div><!--/ .container -->
-</div><!--/ #features -->
+    </div><!-- /accordion-group -->
+</div><!-- Accordion -->
+
 
 
 <section id="showcase" name="showcase"></section>
 <div id="showcase">
     <div class="container">
-        <div class="row">
-            <h1 class="centered">Some Screenshots</h1>
-            <br>
+        <div class="row centered">
+            <h1>FINCODA - user friendly design</h1>
+            <br><br>
             <div class="col-lg-8 col-lg-offset-2">
                 <div id="carousel-example-generic" class="carousel slide">
                     <!-- Indicators -->
@@ -224,53 +175,84 @@
                             <img src="{{'landing_page/img/item-01.png'}}" alt="">
                         </div>
                         <div class="item">
-                            <img src="{{'landing_page/img/item-02.png'}}" alt="">
+                            <img src="{{'landing_page/img/item-01.png'}}" alt="">
                         </div>
+                        <br>
+                        <br>
                     </div>
                 </div>
             </div>
         </div>
         <br>
-        <br>
-        <br>
     </div><!-- /container -->
 </div>
 
 
-<section id="contact" name="contact"></section>
-<div id="footerwrap">
+<div id="contact">
     <div class="container">
-        <div class="col-lg-5">
-            <h3>Contact</h3>
-            <p>
-                Mr. JUSSI RIIHIRANTA,<br/>
-                Project Manager,<br/>
-                urku University of Applied Sciences,<br/>
-                Phone: +358-50-5985731,<br>
-                Turku, Finland
-            </p>
-        </div>
-
-        <div class="col-lg-7">
-            <h3>Drop any question or suggestion.</h3>
-            <br>
-            <form role="form" action="#" method="post" enctype="plain">
-                <div class="form-group">
-                    <label for="name1">Your Name</label>
-                    <input type="name" name="Name" class="form-control" id="name1" placeholder="Your Name">
-                </div>
-                <div class="form-group">
-                    <label for="email1">Email address</label>
-                    <input type="email" name="Mail" class="form-control" id="email1" placeholder="Enter email">
-                </div>
-                <div class="form-group">
-                    <label>Your Text</label>
-                    <textarea class="form-control" name="Message" rows="3"></textarea>
-                </div>
+        <div class="row centered">
+            <h1>FINCODA - we need your opinion</h1>
+            <br><br>
+            <div class="col-lg-6 align-left">
+                <h3>Contact</h3>
+                <p>
+                Mr. Balsam Almurrani<br/>
+                Project Manager<br/>
+                Turku University of Applied Sciences<br/>
+                Turku Finland<br>
+                balsam.almurrani@turkuamk.fi<br>
+                Tel +358-449074990
+                </p>
+            </div>
+            <div class="col-lg-6 align-right">
+                <h3>Drop any question or suggestion</h3>
                 <br>
-                <button type="submit" class="btn btn-large btn-success">SUBMIT</button>
-            </form>
+                <form role="form" action="#" method="post" enctype="plain">
+                    <div class="form-group">
+                        <label for="name1">Your Name</label>
+                        <input type="name" name="Name" class="form-control" id="name1" placeholder="Your Name">
+                    </div>
+                    <div class="form-group">
+                        <label for="email1">Email address</label>
+                        <input type="email" name="Mail" class="form-control" id="email1" placeholder="Enter email">
+                    </div>
+                    <div class="form-group">
+                        <label>Message</label>
+                        <textarea class="form-control" name="Message" rows="3"></textarea>
+                    </div>
+                    <br>
+                    <button type="submit" class="btn btn-large btn-success">SUBMIT</button>
+                </form>
+            </div>
         </div>
+    </div>
+</div>
+
+
+<div class="container">
+<div class="row row-logo">
+    <div class="logos col-sm-4">
+        <a href="http://www2.mmu.ac.uk/" target="_blank"><img src="{{ url('landing_page/img/Logo-MMU.png')}}"></a>
+    </div>
+    <div class="logos col-sm-4">
+        <a href="https://www.international.hu.nl/" target="_blank"><img src="{{ url('landing_page/img/Logo-hu.png')}}"></a>
+    </div>
+    <div class="logos col-sm-4">
+        <a href="https://www.upv.es/" target="_blank"><img src="{{ url('landing_page/img/Logo_Universidad_Politecnica_valencia.png')}}"></a>
+    </div>
+    <div class="logos-long col-sm-6">
+        <a href="http://www.tuas.fi/en/" target="_blank"><img src="{{ url('landing_page/img/Logo-turunamk.jpg')}}"></a>
+    </div>
+    <div class="logos-long col-sm-6">
+        <a href="https://www.haw-hamburg.de/english.html" target="_blank"><img src="{{ url('landing_page/img/01_HAW_Logo_P_web.gif')}}"></a>
+    </div>
+</div>
+
+</div><hr>
+
+<div class="navbar copyright">
+    <div class="pull-left col-sm-6">
+        <h4>Copyright © 2015 FINCODA</h4>
     </div>
 </div>
 
