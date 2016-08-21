1949292d9875ad5a317d4223309274113f5c7287
Shovit thapa <shovitthapa@gmail.com>
2016-08-21 23:50:31 +0300

    some changes

--- database/seeds/IndicatorGroupTableSeeder.php
+++ database/seeds/IndicatorGroupTableSeeder.php
@@ -14,23 +14,22 @@ class IndicatorGroupTableSeeder extends Seeder
     {
         DB::table('indicator_groups')->insert([
             'name' => 'CREATIVITY',
-            'created_at' => \Carbon\Carbon::now()
-        ]);
+                    ]);
         DB::table('indicator_groups')->insert([
             'name' => 'CRITICAL THINKING',
-            'created_at' => \Carbon\Carbon::now()
+
         ]);
         DB::table('indicator_groups')->insert([
             'name' => 'INITIATIVE',
-            'created_at' => \Carbon\Carbon::now()
+
         ]);
         DB::table('indicator_groups')->insert([
             'name' => 'TEAMWORK',
-            'created_at' => \Carbon\Carbon::now()
+
         ]);
         DB::table('indicator_groups')->insert([
             'name' => 'NETWORKING',
-            'created_at' => \Carbon\Carbon::now()
+
         ]);
     }
 }
--- database/seeds/IndicatorTableSeeder.php
+++ database/seeds/IndicatorTableSeeder.php
@@ -17,65 +17,65 @@ class IndicatorTableSeeder extends Seeder
         DB::table('indicators')->insert([
             'indicator' => 'Think differently and adopt different perspectives.',
             'group_id' => 1,
-            'created_at' => \Carbon\Carbon::now()
+
         ]);
         DB::table('indicators')->insert([
             'indicator' => 'Be attentive when others are speaking, and respond effectively to others’ comments during the conversation.',
             'group_id' => 4,
-            'created_at' => \Carbon\Carbon::now()
+
         ]);
         DB::table('indicators')->insert([
             'indicator' => 'Use intuition and own knowledge to start actions.',
             'group_id' => 1,
-            'created_at' => \Carbon\Carbon::now()
+
         ]);
         DB::table('indicators')->insert([
             'indicator' => 'Invite feedback and comments .',
             'group_id' => 4,
-            'created_at' => \Carbon\Carbon::now()
+
         ]);
 
         DB::table('indicators')->insert([
             'indicator' => 'Foster improvements in work organization.',
             'group_id' => 3,
-            'created_at' => \Carbon\Carbon::now()
+
         ]);
 
         DB::table('indicators')->insert([
             'indicator' => 'Obtain constructive comments from colleagues.',
             'group_id' => 4,
-            'created_at' => \Carbon\Carbon::now()
+
 
         ]);
 
         DB::table('indicators')->insert([
             'indicator' => 'Find new ways to implement ideas.',
             'group_id' => 1,
-            'created_at' => \Carbon\Carbon::now()
+
 
         ]);
         DB::table('indicators')->insert([
             'indicator' => 'Identify sources of conflict between oneself and others, or among other people, and to take steps to overcome disharmony',
             'group_id' => 4,
-            'created_at' => \Carbon\Carbon::now()
+
 
         ]);
         DB::table('indicators')->insert([
             'indicator' => 'Take an acceptable level of risk to support new ideas .',
             'group_id' => 3,
-            'created_at' => \Carbon\Carbon::now()
+
 
         ]);
         DB::table('indicators')->insert([
             'indicator' => 'Go beyond expectations in the assignment, task, or job description without being asked  .',
             'group_id' => 3,
-            'created_at' => \Carbon\Carbon::now()
+
 
         ]);
         DB::table('indicators')->insert([
             'indicator' => 'Meet people with different kinds of ideas and perspectives to extend your own knowledge domains .',
             'group_id' => 5,
-            'created_at' => \Carbon\Carbon::now()
+
 
         ]);
 
@@ -84,85 +84,83 @@ class IndicatorTableSeeder extends Seeder
         DB::table('indicators')->insert([
             'indicator' => 'Convince people to support an innovative idea .',
             'group_id' => 3,
-            'created_at' => \Carbon\Carbon::now()
+
 
         ]);
         DB::table('indicators')->insert([
             'indicator' => 'Systematically introduce new ideas into work practices.',
             'group_id' => 3,
-            'created_at' => \Carbon\Carbon::now()
+
 
         ]);
         DB::table('indicators')->insert([
             'indicator' => 'Act quickly and energetically.',
             'group_id' => 3,
-            'created_at' => \Carbon\Carbon::now()
+
 
         ]);
         DB::table('indicators')->insert([
             'indicator' => 'Generate original solutions for problems or to opportunities.',
             'group_id' => 1,
-            'created_at' => \Carbon\Carbon::now()
+
 
         ]);
         DB::table('indicators')->insert([
             'indicator' => 'Use trial and error for problem solving.',
             'group_id' => 2,
-            'created_at' => \Carbon\Carbon::now()
 
         ]);
         DB::table('indicators')->insert([
             'indicator' => 'Develop and experiment with new ways of problem solving .',
             'group_id' => 2,
-            'created_at' => \Carbon\Carbon::now()
+
 
         ]);
         DB::table('indicators')->insert([
             'indicator' => 'Acquire, assimilate, transform and exploit external knowledge to establish, manage and learn from informal organisational ties .',
             'group_id' => 5,
-            'created_at' => \Carbon\Carbon::now()
+
 
         ]);
         DB::table('indicators')->insert([
             'indicator' => 'Challenge the status quo.',
             'group_id' => 2,
-            'created_at' => \Carbon\Carbon::now()
 
         ]);
         DB::table('indicators')->insert([
             'indicator' => 'Face the task from different points of view.',
             'group_id' => 2,
-            'created_at' => \Carbon\Carbon::now()
+
 
 
         ]);
         DB::table('indicators')->insert([
             'indicator' => 'Make suggestions to improve current process products or services.',
             'group_id' => 1,
-            'created_at' => \Carbon\Carbon::now()
+
 
         ]);
         DB::table('indicators')->insert([
             'indicator' => 'Present novel ideas.',
             'group_id' => 1,
-            'created_at' => \Carbon\Carbon::now()
+
 
         ]);
         DB::table('indicators')->insert([
             'indicator' => 'Forecast impact on users.',
             'group_id' =>2,
-            'created_at' => \Carbon\Carbon::now()
+
 
         ]);
         DB::table('indicators')->insert([
             'indicator' => 'Show inventiveness in using resources.',
             'group_id' => 1,
-            'created_at' => \Carbon\Carbon::now()
+
         ]);
         DB::table('indicators')->insert([
             'indicator' => 'Search out new working methods, techniques or instruments.',
             'group_id' => 1,
-            'created_at' => \Carbon\Carbon::now()
+
         ]);
 
 
@@ -170,55 +168,53 @@ class IndicatorTableSeeder extends Seeder
         DB::table('indicators')->insert([
             'indicator' => 'Provide constructive feedback, cooperation, coaching or help to team colleagues.',
             'group_id' => 4,
-            'created_at' => \Carbon\Carbon::now()
+
 
         ]);
         DB::table('indicators')->insert([
             'indicator' => 'Work well with others, understanding their needs and being sympathetic with them.',
             'group_id' => 4,
-            'created_at' => \Carbon\Carbon::now()
+
 
         ]);
         DB::table('indicators')->insert([
             'indicator' => 'Share timely information with the appropriate stakeholders.',
             'group_id' => 4,
-            'created_at' => \Carbon\Carbon::now()
+
 
         ]);
         DB::table('indicators')->insert([
             'indicator' => 'Consult about essential changes .',
             'group_id' => 4,
-            'created_at' => \Carbon\Carbon::now()
+
 
         ]);
         DB::table('indicators')->insert([
             'indicator' => 'Build relationships outside the team/organization.',
             'group_id' => 5,
-            'created_at' => \Carbon\Carbon::now()
 
         ]);
         DB::table('indicators')->insert([
             'indicator' => 'Refine ideas into a useful form.',
             'group_id' => 1,
-            'created_at' => \Carbon\Carbon::now()
+
 
         ]);
         DB::table('indicators')->insert([
             'indicator' => 'Engage outsiders of the core work group from the beginning.',
             'group_id' => 5,
-            'created_at' => \Carbon\Carbon::now()
 
         ]);
         DB::table('indicators')->insert([
             'indicator' => 'Ask “Why?” and “Why not?” and “What if?” with a purpose',
             'group_id' => 2,
-            'created_at' => \Carbon\Carbon::now()
+
 
         ]);
         DB::table('indicators')->insert([
             'indicator' => 'Work in multidisciplinary environments ',
             'group_id' => 5,
-            'created_at' => \Carbon\Carbon::now()
+           
 
         ]);
 
