f57922e4a67569a5368f057c050ed0d0ed5a6aa1
DuyLeNguyen <duy.lenguyen@edu.turkuamk.fi>
2017-01-21 16:56:11 +0200

    First PDF version

--- resources/views/survey/resultForSpecialPdfOverview.blade.php
+++ resources/views/survey/resultForSpecialPdfOverview.blade.php
@@ -14,10 +14,10 @@
 
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
-    
+
     <!-- Ionicons -->
     <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
-    
+
     <link rel="stylesheet" href="{{URL::asset('css/custom.css')}}">
 
     <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
@@ -29,10 +29,10 @@
     <link rel="stylesheet" href="{{URL::asset('css/AdminLTE.min.css')}}">
 
     <link rel="stylesheet" href="{{URL::asset('css/skins/skin-blue.min.css')}}">
-    
+
     <script src="{{URL::asset('js/app.min.js')}}"></script>
     <script src="https://cdn.ckeditor.com/4.4.3/standard/ckeditor.js"></script>
-    
+
 
     <!-- Bootstrap WYSIHTML5 -->
     <script src="{{URL::asset('input-mask/jquery.inputmask.js')}}"></script>
@@ -45,12 +45,6 @@
 </head>
 
 <body class="hold-transition skin-blue sidebar-mini">
-            <div class="box-header with-border">
-                <h3 class="box-title"><b>Survey Result.</b></h3>
-                <p><i>Below is the information about the pending survey you requested.
-                        You can make changes or abort it before it is open to the participants.</i></p>
-
-            </div>
 
             @include('message.fail')
             @include('message.errors_head')
@@ -61,11 +55,11 @@
                     <div class="panel panel-default">
                         <div class="panel-body">
                             <p>
-                                <h2>Survey Results</h2><br>
+                                <h2 style="text-align:center;">Survey Results</h2><br>
                                 <!-- <label id="surveyId">{!! $survey->title !!}</label> -->
-                                <ul>
+                                <ul class="survey-info">
 								  {{App::setLocale(Session::get('language'))}}
-								  <li><h5><label>Id : </label> {!! $survey->id !!}</h5></li>
+								                  <li><h5><label>Id : </label> {!! $survey->id !!}</h5></li>
                                   <li><h5 class="text-capitalize"><label>Title : </label> {!! $survey->title !!}</h5></li>
                                   <li><h5 class="text-capitalize"><label>Type : </label> {!! \App\Survey_Type::find($survey->type_id)->name !!}</h5></li>
                                   <li><h5><label>Start time : </label> {!! $survey->start_time !!}</h5></li>
@@ -75,25 +69,14 @@
                                 </ul>
 
                                 @role ('special')
-                                <ul class="nav nav-tabs">
-                                  <li class="active"><a data-toggle="tab" href="#overview">Overview</a></li>
-                                </ul>
 
                                 <div class="tab-content">
                                   <div class="tab-pane fade in active">
-                                    <div class="report-caption">
-                                      <h4><b>Description</b></h4>
-                                      <p>The bar graph shows your answers in this survey.
-                          							The table underneath this graph displayed the same data in table format.
-                          							Alternate between the two buttons to view your scores only or in comparison with the group average.
-                          							Only your score are shown by default.</p>
-                                    </div>
 
-                                    
                                     @role ('special')
                                     <!-- Company average graph -->
 				<div class="canvas-holder" style="width: 818px; height: 409px;">
-				<h4><b>Indicators Chart</b></h4>
+				<h3 style="text-align:center;"><b>Indicators Chart</b></h3>
 									@if(count($surveyGroupAveragePerIndicatorAllUsers)==34)
                                     <canvas id="companyAverage" width="800" height="400"></canvas>
                                     <script src="http://localhost/fincoda-phase2-complete/public/js/displayChart.js">
@@ -105,36 +88,26 @@
                                                 "Ind 13", "Ind 14", "Ind 15", "Ind 16", "Ind 17", "Ind 18", "Ind 19", "Ind 20", "Ind 21", "Ind 22", "Ind 23", "Ind 24",
                                                 "Ind 25", "Ind 26", "Ind 27", "Ind 28", "Ind 29", "Ind 30", "Ind 31", "Ind 32", "Ind 33", "Ind 34"],
                                         'Company average score of each indicator',
-									    [{!!$surveyGroupAveragePerIndicatorAllUsers[0]->Group_Average!!}, {!!$surveyGroupAveragePerIndicatorAllUsers[1]->Group_Average!!}, {!!$surveyGroupAveragePerIndicatorAllUsers[2]->Group_Average!!}, {!!$surveyGroupAveragePerIndicatorAllUsers[3]->Group_Average!!}, {!!$surveyGroupAveragePerIndicatorAllUsers[4]->Group_Average!!},
-                                          {!!$surveyGroupAveragePerIndicatorAllUsers[5]->Group_Average!!}, {!!$surveyGroupAveragePerIndicatorAllUsers[6]->Group_Average!!}, {!!$surveyGroupAveragePerIndicatorAllUsers[7]->Group_Average!!}, {!!$surveyGroupAveragePerIndicatorAllUsers[8]->Group_Average!!}, {!!$surveyGroupAveragePerIndicatorAllUsers[9]->Group_Average!!},
-                                           {!!$surveyGroupAveragePerIndicatorAllUsers[10]->Group_Average!!}, {!!$surveyGroupAveragePerIndicatorAllUsers[11]->Group_Average!!}, {!!$surveyGroupAveragePerIndicatorAllUsers[12]->Group_Average!!}, {!!$surveyGroupAveragePerIndicatorAllUsers[13]->Group_Average!!}, {!!$surveyGroupAveragePerIndicatorAllUsers[14]->Group_Average!!},
-                                            {!!$surveyGroupAveragePerIndicatorAllUsers[15]->Group_Average!!}, {!!$surveyGroupAveragePerIndicatorAllUsers[16]->Group_Average!!}, {!!$surveyGroupAveragePerIndicatorAllUsers[17]->Group_Average!!}, {!!$surveyGroupAveragePerIndicatorAllUsers[18]->Group_Average!!}, {!!$surveyGroupAveragePerIndicatorAllUsers[19]->Group_Average!!},
-                                              {!!$surveyGroupAveragePerIndicatorAllUsers[20]->Group_Average!!}, {!!$surveyGroupAveragePerIndicatorAllUsers[21]->Group_Average!!}, {!!$surveyGroupAveragePerIndicatorAllUsers[22]->Group_Average!!}, {!!$surveyGroupAveragePerIndicatorAllUsers[23]->Group_Average!!}, {!!$surveyGroupAveragePerIndicatorAllUsers[24]->Group_Average!!},
-                                                {!!$surveyGroupAveragePerIndicatorAllUsers[25]->Group_Average!!}, {!!$surveyGroupAveragePerIndicatorAllUsers[26]->Group_Average!!}, {!!$surveyGroupAveragePerIndicatorAllUsers[27]->Group_Average!!}, {!!$surveyGroupAveragePerIndicatorAllUsers[28]->Group_Average!!}, {!!$surveyGroupAveragePerIndicatorAllUsers[29]->Group_Average!!},
-                                                  {!!$surveyGroupAveragePerIndicatorAllUsers[30]->Group_Average!!}, {!!$surveyGroupAveragePerIndicatorAllUsers[31]->Group_Average!!}, {!!$surveyGroupAveragePerIndicatorAllUsers[32]->Group_Average!!}, {!!$surveyGroupAveragePerIndicatorAllUsers[33]->Group_Average!!}],
+                                        [{!!number_format((float)$surveyGroupAveragePerIndicatorAllUsers[0]->Group_Average,2,'.','')!!}, {!!number_format((float)$surveyGroupAveragePerIndicatorAllUsers[1]->Group_Average,2,'.','')!!}, {!!number_format((float)$surveyGroupAveragePerIndicatorAllUsers[2]->Group_Average,2,'.','')!!}, {!!number_format((float)$surveyGroupAveragePerIndicatorAllUsers[3]->Group_Average,2,'.','')!!}, {!!number_format((float)$surveyGroupAveragePerIndicatorAllUsers[4]->Group_Average,2,'.','')!!},
+                                          {!!number_format((float)$surveyGroupAveragePerIndicatorAllUsers[5]->Group_Average,2,'.','')!!}, {!!number_format((float)$surveyGroupAveragePerIndicatorAllUsers[6]->Group_Average,2,'.','')!!}, {!!number_format((float)$surveyGroupAveragePerIndicatorAllUsers[7]->Group_Average,2,'.','')!!}, {!!number_format((float)$surveyGroupAveragePerIndicatorAllUsers[8]->Group_Average,2,'.','')!!}, {!!number_format((float)$surveyGroupAveragePerIndicatorAllUsers[9]->Group_Average,2,'.','')!!},
+                                           {!!number_format((float)$surveyGroupAveragePerIndicatorAllUsers[10]->Group_Average,2,'.','')!!}, {!!number_format((float)$surveyGroupAveragePerIndicatorAllUsers[11]->Group_Average,2,'.','')!!}, {!!number_format((float)$surveyGroupAveragePerIndicatorAllUsers[12]->Group_Average,2,'.','')!!}, {!!number_format((float)$surveyGroupAveragePerIndicatorAllUsers[13]->Group_Average,2,'.','')!!}, {!!number_format((float)$surveyGroupAveragePerIndicatorAllUsers[14]->Group_Average,2,'.','')!!},
+                                            {!!number_format((float)$surveyGroupAveragePerIndicatorAllUsers[15]->Group_Average,2,'.','')!!}, {!!number_format((float)$surveyGroupAveragePerIndicatorAllUsers[16]->Group_Average,2,'.','')!!}, {!!number_format((float)$surveyGroupAveragePerIndicatorAllUsers[17]->Group_Average,2,'.','')!!}, {!!number_format((float)$surveyGroupAveragePerIndicatorAllUsers[18]->Group_Average,2,'.','')!!}, {!!number_format((float)$surveyGroupAveragePerIndicatorAllUsers[19]->Group_Average,2,'.','')!!},
+                                              {!!number_format((float)$surveyGroupAveragePerIndicatorAllUsers[20]->Group_Average,2,'.','')!!}, {!!number_format((float)$surveyGroupAveragePerIndicatorAllUsers[21]->Group_Average,2,'.','')!!}, {!!number_format((float)$surveyGroupAveragePerIndicatorAllUsers[22]->Group_Average,2,'.','')!!}, {!!number_format((float)$surveyGroupAveragePerIndicatorAllUsers[23]->Group_Average,2,'.','')!!}, {!!number_format((float)$surveyGroupAveragePerIndicatorAllUsers[24]->Group_Average,2,'.','')!!},
+                                                {!!number_format((float)$surveyGroupAveragePerIndicatorAllUsers[25]->Group_Average,2,'.','')!!}, {!!number_format((float)$surveyGroupAveragePerIndicatorAllUsers[26]->Group_Average,2,'.','')!!}, {!!number_format((float)$surveyGroupAveragePerIndicatorAllUsers[27]->Group_Average,2,'.','')!!}, {!!number_format((float)$surveyGroupAveragePerIndicatorAllUsers[28]->Group_Average,2,'.','')!!}, {!!number_format((float)$surveyGroupAveragePerIndicatorAllUsers[29]->Group_Average,2,'.','')!!},
+                                                  {!!number_format((float)$surveyGroupAveragePerIndicatorAllUsers[30]->Group_Average,2,'.','')!!}, {!!number_format((float)$surveyGroupAveragePerIndicatorAllUsers[31]->Group_Average,2,'.','')!!}, {!!number_format((float)$surveyGroupAveragePerIndicatorAllUsers[32]->Group_Average,2,'.','')!!}, {!!number_format((float)$surveyGroupAveragePerIndicatorAllUsers[33]->Group_Average,2,'.','')!!}],
                                     	'rgba(0,0,255,1)'
                                       );
-                                    </script>                                    
+                                    </script>
 
-				
+
 									@else
-										<div>You have no surveys results to display or your indicators count is not equal 34</div>
+										<h3><b>You have no surveys results to display or your indicators count is not equal 34</b></h3>
 									@endif
 				</div>
-				
-				    <div>
-                                      @include ('survey.resultContent.scoreTable')
-                                    </div>
-				    
 
-		
-				    <div>
-                                      @include ('survey.resultContent.surveyScorePerIndicatorGroup')
-                                    </div>
-
-                                    <div>
+				                            <div>
                                       <table class="table table-bordered table-striped text-center">
-                                        <h4><b>Indicators Table</b></h4>
+                                        <h3 style="text-align:center;"><b>Indicators Table</b></h3>
                                           <thead>
                                             <tr>
                                                 <th>Indicator ID</th>
@@ -144,44 +117,74 @@
                                           </thead>
                                           <tbody>
                                             @if(count($surveyScoreAllUsers)==0)
-                                              <div>You have no surveys results to display</div>
+                                              <h3><b>You have no surveys results to display</b></h3>
                                             @else
                                               @foreach($surveyGroupAveragePerIndicatorAllUsers as $result)
                                                 <tr>
                                                   <td>{!! $result->Indicator_ID !!}</td>
                                                   <td>{{Lang::get('indicators.'.$result->Indicator_ID,array(),App::getLocale())}}</td>
-                                                  <td>{!! $result->Group_Average !!}</td>
+                                                  <td>{!! number_format((float)$result->Group_Average,2,'.','') !!}</td>
                                                 </tr>
                                               @endforeach
                                             @endif
                                           </tbody>
                                       </table>
                                     </div>
+                                    <br>
+        <h3 style="text-align:center;"><b>Company average score per dimension</b></h3>
 				<div class="canvas-holder" style="width: 818px; height: 409px;">
 									@if(count($surveyScorePerIndicatorGroup)==5)
 				    <canvas id="indicatorGroupAverage" width="800" height="400"></canvas>
                                     <script src="http://localhost/fincoda-phase2-complete/public/js/displayChart.js">
                                     </script>
 				    <script>
-                                      createChart(
-                                        document.getElementById("indicatorGroupAverage"),
-                                        ["CREATIVITY", "CRITICAL THINKING", "INITIATIVE", "TEAMWORK", "NETWORKING",],
-                                        'Company average score of each dimension',
-                                        [{!!$surveyScorePerIndicatorGroup[0]->Indicator_Group_Average!!}, {!!$surveyScorePerIndicatorGroup[1]->Indicator_Group_Average!!}, {!!$surveyScorePerIndicatorGroup[2]->Indicator_Group_Average!!},
-                                          {!!$surveyScorePerIndicatorGroup[3]->Indicator_Group_Average!!}, {!!$surveyScorePerIndicatorGroup[4]->Indicator_Group_Average!!}],
-                                        'rgba(0,0,255,1)'
-                                      );
+            var chartArea = document.getElementById('indicatorGroupAverage');
+            var datasetMinCompany = {
+              label: 'Minimum Average Score Each Dimension',
+              data: [
+                      {!! number_format($surveyScoreGroupAvgPerIndicatorGroupMinAndMax[0]->Minimum_User_Indicator_Group_Average, 2, '.','') !!},
+                      {!!number_format((float)$surveyScoreGroupAvgPerIndicatorGroupMinAndMax[1]->Minimum_User_Indicator_Group_Average,2,'.','')!!},
+                      {!!number_format((float)$surveyScoreGroupAvgPerIndicatorGroupMinAndMax[2]->Minimum_User_Indicator_Group_Average,2,'.','')!!},
+                      {!!number_format((float)$surveyScoreGroupAvgPerIndicatorGroupMinAndMax[3]->Minimum_User_Indicator_Group_Average,2,'.','')!!},
+                      {!!number_format((float)$surveyScoreGroupAvgPerIndicatorGroupMinAndMax[4]->Minimum_User_Indicator_Group_Average,2,'.','')!!}
+                    ],
+               backgroundColor: 'rgba(255,0,0,1)'
+            };
+            var datasetMaxCompany = {
+              label: 'Maximum Average Score Each Dimension',
+              data: [
+                {!!number_format((float)$surveyScoreGroupAvgPerIndicatorGroupMinAndMax[0]->Maximum_User_Indicator_Group_Average,2,'.','')!!},
+                {!!number_format((float)$surveyScoreGroupAvgPerIndicatorGroupMinAndMax[1]->Maximum_User_Indicator_Group_Average,2,'.','')!!},
+                {!!number_format((float)$surveyScoreGroupAvgPerIndicatorGroupMinAndMax[2]->Maximum_User_Indicator_Group_Average,2,'.','')!!},
+                {!!number_format((float)$surveyScoreGroupAvgPerIndicatorGroupMinAndMax[3]->Maximum_User_Indicator_Group_Average,2,'.','')!!},
+                {!!number_format((float)$surveyScoreGroupAvgPerIndicatorGroupMinAndMax[4]->Maximum_User_Indicator_Group_Average,2,'.','')!!}
+              ],
+              backgroundColor: 'rgba(255,255,0,1)'
+            };
+            var datasetAvgCompany = {
+              label: 'Company Average Score Each Dimension',
+              data: [
+                {!!number_format((float)$surveyScorePerIndicatorGroup[0]->Indicator_Group_Average,2,'.','')!!}, {!!number_format((float)$surveyScorePerIndicatorGroup[1]->Indicator_Group_Average,2,'.','')!!}, {!!number_format((float)$surveyScorePerIndicatorGroup[2]->Indicator_Group_Average,2,'.','')!!},
+                  {!!number_format((float)$surveyScorePerIndicatorGroup[3]->Indicator_Group_Average,2,'.','')!!}, {!!number_format((float)$surveyScorePerIndicatorGroup[4]->Indicator_Group_Average,2,'.','')!!}
+              ],
+              backgroundColor: 'rgba(0,0,255,1)'
+            };
+            var labelArr = ["CREATIVITY", "CRITICAL THINKING", "INITIATIVE", "TEAMWORK", "NETWORKING"];
+            createMaxMinChart(chartArea, labelArr, datasetMinCompany, datasetAvgCompany, datasetMaxCompany);
                                     </script>
-				  
+
 									@else
-										<div>You have no surveys results to display or your indicators group count is not equal 5</div>
+										<h3><b>You have no surveys results to display or your indicators group count is not equal 5</b></h3>
 									@endif
 				</div>
 
+        <div>
+          @include ('survey.resultContent.surveyScorePerIndicatorGroup')
+        </div>
 
-                                      
+
                                   </div>
-				  
+
                                   @endrole
 
                                 </div>
