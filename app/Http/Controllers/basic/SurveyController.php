99afe5d9e3fe11fd95a6302dd8bf53107fa2b90f
Kawalya <davis.kawalya@edu.turkuamk.fi>
2017-05-13 13:26:03 +0300

    load images through url asset in the pdf

--- app/Http/Controllers/basic/SurveyController.php
+++ app/Http/Controllers/basic/SurveyController.php
@@ -147,7 +147,7 @@ class SurveyController extends Controller
                     ->withInput();
 				}
 				
-				$view = View::make('survey.resultForAdminPdfOverView',compact('survey','surveyScoreAllUsers','surveyGroupAveragePerIndicatorAllUsers',
+				$view = View::make('survey.resultForUserPdfOverView',compact('survey','surveyScoreAllUsers','surveyGroupAveragePerIndicatorAllUsers',
 									'surveyScorePerIndicatorGroup','surveyScoreGroupAvgPerIndicatorGroup','surveyScoreGroupAvgPerIndicatorGroupMinAndMax',
 									'participants','company','company_profile','answers'))->render();
 				$pdf = App::make('snappy.pdf.wrapper');
@@ -266,7 +266,7 @@ class SurveyController extends Controller
                     ->withInput();
 				}
 				
-				$view = PDF::loadView('survey.resultForAdminPdfOverView',
+				$view = PDF::loadView('survey.resultForUserPdfOverView',
 										compact('survey','$surveyScoreAllUsersCheckThreeParticipants','surveyScoreAllUsers',
 											'surveyGroupAveragePerIndicatorAllUsers','surveyScorePerIndicatorGroup',
 											'surveyScoreGroupAvgPerIndicatorGroup','surveyScoreGroupAvgPerIndicatorGroupMinAndMax',
