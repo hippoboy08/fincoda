c6f2b5285d6576d8415593a0b626ab09196ef733
Khoi Huynh <khoi.huynh@edu.turkuamk.fi>
2017-10-09 11:50:04 +0300

    Show all Min, Avg, Max in the Dimension table for admin and special survey result overview tab if they are the owner of the survey.

--- resources/views/survey/resultContent/surveyScorePerIndicatorGroup.blade.php
+++ resources/views/survey/resultContent/surveyScorePerIndicatorGroup.blade.php
@@ -4,19 +4,23 @@
   <thead>
     <tr>
       <th>Dimension</th>
+      <th>Dimension Minimum</th>
       <th>Dimension Average</th>
+      <th>Dimension Maximum</th>
     </tr>
   </thead>
   <tbody>
-    @if(count($surveyScorePerIndicatorGroup)==0)
+    @if(count($surveyScoreGroupAvgPerIndicatorGroupMinAndMax)==0)
       <div>You have no surveys results to Display</div>
     @else
-      @foreach($surveyScorePerIndicatorGroup as $result)
+      @for ($index = 0; $index < count($surveyScoreGroupAvgPerIndicatorGroupMinAndMax); $index++)
         <tr>
-          <td>{!! $result->Indicator_Group !!}</td>
-          <td>{!! number_format((float)$result->Indicator_Group_Average,2,'.','') !!}</td>
+          <td>{!! $surveyScoreGroupAvgPerIndicatorGroupMinAndMax[$index]->Indicator_Group !!}</td>
+          <td>{!! number_format((float)$surveyScoreGroupAvgPerIndicatorGroupMinAndMax[$index]->Minimum_User_Indicator_Group_Average,2,'.','') !!}</td>
+          <td>{!! number_format((float)$surveyScorePerIndicatorGroup[$index]->Indicator_Group_Average,2,'.','') !!}</td>
+          <td>{!! number_format((float)$surveyScoreGroupAvgPerIndicatorGroupMinAndMax[$index]->Maximum_User_Indicator_Group_Average,2,'.','') !!}</td>
         </tr>
-      @endforeach
+      @endfor
     @endif
   </tbody>
 </table>
