266d9eb5d97cd0f7c0c707499828a76d0c24f80a
DuyLeNguyen <duy.lenguyen@edu.turkuamk.fi>
2017-12-14 15:41:52 +0200

    Fix Reset Password Issues, minify admin and special PDF files

--- app/Http/Controllers/Auth/PasswordController.php
+++ app/Http/Controllers/Auth/PasswordController.php
@@ -26,7 +26,7 @@ class PasswordController extends Controller
     use ResetsPasswords;
 
     protected $redirectTo = '/login';
-	
+
 	public function reset(Request $request)
     {
 		$this->validate(
@@ -63,43 +63,43 @@ class PasswordController extends Controller
         }
     }
 
-    public function reset(Request $request)
-    {
-		$this->validate(
-            $request,
-            $this->getResetValidationRules(),
-            $this->getResetValidationMessages(),
-            $this->getResetValidationCustomAttributes()
-        );
-
-        $credentials = $this->getResetCredentials($request);
-
-        $broker = $this->getBroker();
-
-        $response = Password::broker($broker)->reset($credentials, function ($user, $password) {
-            $this->resetPassword($user, $password);
-        });
-
-        switch ($response) {
-            case Password::PASSWORD_RESET:
-                //dd(Auth::User()->name);
-					 if(Auth::User()->hasRole('admin')){
-						return redirect('admin');
-					   }
-						elseif(Auth::User()->hasRole('special')){
-							return redirect('special');
-						}
-						elseif(Auth::User()->hasRole('external')){
-								return redirect('external');
-							}
-						else{
-							return redirect('basic');
-						}
-				return $this->getResetSuccessResponse($response);
-            default:
-                return $this->getResetFailureResponse($request, $response);
-        }
-    }
+    // public function reset(Request $request)
+    // {
+		// $this->validate(
+    //         $request,
+    //         $this->getResetValidationRules(),
+    //         $this->getResetValidationMessages(),
+    //         $this->getResetValidationCustomAttributes()
+    //     );
+    //
+    //     $credentials = $this->getResetCredentials($request);
+    //
+    //     $broker = $this->getBroker();
+    //
+    //     $response = Password::broker($broker)->reset($credentials, function ($user, $password) {
+    //         $this->resetPassword($user, $password);
+    //     });
+    //
+    //     switch ($response) {
+    //         case Password::PASSWORD_RESET:
+    //             //dd(Auth::User()->name);
+		// 			 if(Auth::User()->hasRole('admin')){
+		// 				return redirect('admin');
+		// 			   }
+		// 				elseif(Auth::User()->hasRole('special')){
+		// 					return redirect('special');
+		// 				}
+		// 				elseif(Auth::User()->hasRole('external')){
+		// 						return redirect('external');
+		// 					}
+		// 				else{
+		// 					return redirect('basic');
+		// 				}
+		// 		return $this->getResetSuccessResponse($response);
+    //         default:
+    //             return $this->getResetFailureResponse($request, $response);
+    //     }
+    // }
 
 
     /**
